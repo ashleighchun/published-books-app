b0c6c8798b55c1d9b305d272c7127b3f0057073c
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-06-10 17:48:35 -0700

    set up books controller methods

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,71 @@
 class BooksController < ApplicationController
 
   get '/books' do
-    #redirect '/login' if !logged_in?
+    redirect '/login' if !logged_in?
     @books = current_reader.books
     erb :'books/books'
   end
-end
+
+
+  get '/books/new' do
+      redirect '/login' if !logged_in?
+      erb :'books/new'
+    end
+
+    post '/books' do
+      redirect '/login' if !logged_in?
+      if params[:content] == ""
+        redirect to "/books/new"
+      else
+        @book = current_user.books.build(content: params[:content])
+        if @book.save
+          redirect to "/books/#{@book.id}"
+        else
+          redirect to "/books/new"
+        end
+      end
+    end
+
+    get '/books/:id' do
+      redirect '/login' if !logged_in?
+      @book = Book.find_by_id(params[:id])
+      erb :'books/show'
+     end
+
+    get '/books/:id/edit' do
+      redirect '/login' if !logged_in?
+      @book = Book.find_by_id(params[:id])
+      if @book && @book.user == current_user
+        erb :'books/edit'
+      else
+        redirect to '/books'
+      end
+    end
+
+    patch '/books/:id' do
+      redirect '/login' if !logged_in?
+      if params[:content] == ""
+        redirect to "/books/#{params[:id]}/edit"
+      else
+        @book = Book.find_by_id(params[:id])
+        if @book && @book.user == current_user
+          if @book.update(content: params[:content])
+            redirect to "/books/#{@book.id}"
+          else
+            redirect to "/books/#{@book.id}/edit"
+          end
+        else
+          redirect to 'books'
+        end
+      end
+    end
+
+    delete '/books/:id/delete' do
+      redirect '/login' if !logged_in?
+      @book = Book.find_by_id(params[:id])
+      if @book && @book.user == current_user
+        @book.delete
+      end
+      redirect to '/books'
+    end
+  end
--- app/controllers/readers_controller.rb
+++ app/controllers/readers_controller.rb
@@ -1,4 +1,5 @@
 class ReadersController < ApplicationController
+
   get 'readers/:slug' do
     @reader = Reader.find_by_slug(params[:slug])
     erb :'users/show'
@@ -12,34 +13,33 @@ class ReadersController < ApplicationController
    end
 
    post '/signup' do
-      if params[:name] == "" || params[:email] == "" || params[:password] ==""
-        redirect "/signup"
-      else
-        @reader = Reader.create(name: params[:name], email: params[:email], password: params[:password])
-        session[:reader_id] = @reader.id
-        redirect '/books'
-     end
-   end
+    if params[:name] == "" || params[:email] == "" || params[:password] ==""
+      redirect "/signup"
+    else
+      @reader = Reader.create(name: params[:name], email: params[:email], password: params[:password])
+      session[:reader_id] = @reader.id
+      redirect '/books'
+    end
+  end
 
-   get '/login' do
-       redirect '/books' if logged_in?
-       erb :'readers/login'
-     end
+  get '/login' do
+    redirect '/books' if logged_in?
+    erb :'readers/login'
+  end
 
-     post '/login' do
-       @reader = reader.find_by(:name => params[:name])
-       if @reader && @reader.authenticate(params[:password])
-         session[:reader_id] = @reader.id
-         redirect '/books'
-       else
-         redirect '/signup'
-       end
-     end
+  post '/login' do
+    @reader = reader.find_by(:name => params[:name])
+    if @reader && @reader.authenticate(params[:password])
+      session[:reader_id] = @reader.id
+      redirect '/books'
+    else
+      redirect '/signup'
+    end
+  end
 
-     get '/logout' do
-       redirect '/books' if !logged_in?
-       session.clear
-       redirect '/login'
-     end
-
+  get '/logout' do
+    redirect '/books' if !logged_in?
+    session.clear
+    redirect '/login'
+  end
 end

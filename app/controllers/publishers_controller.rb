e8f632d186c9fb90c2b9bd15c690e5a2e12404b1
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-06-12 08:59:29 -0700

    add publisher controller methods

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -1,7 +1,21 @@
 class PublishersController < ApplicationController
 
   get '/publishers' do
-    @books = current_book.publishers
-    erb :'books/publishers'
+    if logged_in?
+      @books = current_book.publishers.uniq
+      erb :'books/publishers'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/publishers/:id' do
+    if logged_in?
+      @publisher = Publisher.find_by_id(params[:id])
+      @books = @publisher.books.select { |dream| dream.user_id == current_user.id}
+      erb :'books/show'
+    else
+      redirect '/login'
+    end 
   end
 end

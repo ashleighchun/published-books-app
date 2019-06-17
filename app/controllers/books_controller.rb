ce9207ecf43ddd9a656e32acc359cd2378b352c8
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-06-16 22:38:03 -0700

    add sessions login folder and associated routes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,7 @@ class BooksController < ApplicationController
 
   get '/books/new' do #-------new first prevents a false match. when it gets a request it looks for a route starting from the top and going down. if it gets to id before new it will assume new is the id and will go into show instead of new
     redirect_if_not_logged_in
+    @reader = current_reader
     erb :'books/new'
   end
 
@@ -30,7 +31,11 @@ class BooksController < ApplicationController
   get '/books/:id' do
     redirect_if_not_logged_in
     @book = Book.find_by_id(params[:id])
-    erb :'books/show'
+    if @book && @book.reader == current_reader
+      erb :'books/show'
+    else
+      redirect to '/books'
+    end
   end
 
   get '/books/:id/edit' do
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,25 @@
 class SessionsController<ApplicationController
+  get '/sessions/login' do
+    if logged_in?
+      redirect to '/welcome'
+    else
+      erb :'sessions/login'
+    end
+  end
 
+  post '/sessions/login' do
+    user = User.find_by(name: params[:name])
+
+    if reader && reader.authenticate(params[:password])
+      session[:reader_id] = reader.id
+      redirect to '/books'
+    else
+      redirect to '/readers/signup'
+    end
+  end
+
+  get '/sessions/logout' do
+    session.clear
+    redirect to '/'
+  end
 end
